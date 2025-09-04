483bf87a41a973f764a4625af7f8e911281ae7df
Oliver766 <67057688+Oliver766@users.noreply.github.com>
2025-09-04 15:10:46 +0100

    updated changes

--- Assets/Scripts/Tilemap/Elevation_Entry.cs
+++ Assets/Scripts/Tilemap/Elevation_Entry.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class Elevation_Entry : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Collider2D[] mountainCollider;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            foreach (Collider2D mountain in mountainCollider)
+            {
+                mountain.enabled = false;
+            }
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15;
+
+        }
     }
 }
