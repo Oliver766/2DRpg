04935a693a1dcb319b39a838129971377b827dcb
Oliver766 <67057688+Oliver766@users.noreply.github.com>
2025-09-11 11:32:24 +0100

    push

--- Assets/Scripts/Tilemap/Elevation_Exit.cs
+++ Assets/Scripts/Tilemap/Elevation_Exit.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 
-public class Elevation_Entry : MonoBehaviour
+public class Elevation_Exit : MonoBehaviour
 {
-    public Collider2D[] mountainCollider;
+     public Collider2D[] mountainCollider;
     public Collider2D[] BoundaryCollider;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -11,13 +11,13 @@ public class Elevation_Entry : MonoBehaviour
         {
             foreach (Collider2D mountain in mountainCollider)
             {
-                mountain.enabled = false;
+                mountain.enabled = true;
             }
             foreach (Collider2D boundary in BoundaryCollider)
             {
-                boundary.enabled = true;
+                boundary.enabled = false;
             }
-            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15;
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 10;
 
         }
     }
